1b8d388c00344f97e742afe4c4aecea47b78e26f
jonathangomz <jonicgp97@gmail.com>
2022-02-23 22:08:46 -0600

    Renames BaseFields to Object

--- lib/src/notion/objects/object.dart
+++ lib/src/notion/objects/object.dart
@@ -2,7 +2,7 @@ import '../../utils/utils.dart';
 import '../general/notion_types.dart';
 
 /// A base representation of the base properties of almost any Notion object.
-class BaseFields {
+class Object {
   /// The object type.
   ObjectTypes object;
 
@@ -10,10 +10,10 @@ class BaseFields {
   String id;
 
   /// The creation time of the object.
-  String createdTime;
+  DateTime? createdTime;
 
   /// The last edited time of the object.
-  String lastEditedTime;
+  DateTime? lastEditedTime;
 
   /// The string value of the object type.
   String get strObject => objectTypeToString(object);
@@ -23,15 +23,15 @@ class BaseFields {
   /// Can receive the [object], the [id], the [createdTime] and the [lastEditedTime] of the object.
   ///
   /// **Note:** This class is mainly (if no only) used by extending it.
-  BaseFields({
+  Object({
     this.object: ObjectTypes.Object,
     this.id: '',
-    this.createdTime: '',
-    this.lastEditedTime: '',
+    DateTime? this.createdTime,
+    DateTime? this.lastEditedTime,
   });
 
   /// Map the properties from a [json] map.
-  BaseFields.fromJson(Map<String, dynamic> json)
+  Object.fromJson(Map<String, dynamic> json)
       : this.object = stringToObjectType(json['object']),
         this.id = json['id'] ?? '',
         this.createdTime = json['created_time'] ?? '',
@@ -41,8 +41,8 @@ class BaseFields {
   ///
   /// This function is used to set the base properties from the constructor of the class in which it is inherited.
   void setBaseProperties({
-    required String createdTime,
-    required String lastEditedTime,
+    DateTime? createdTime,
+    DateTime? lastEditedTime,
   }) {
     this.createdTime = createdTime;
     this.lastEditedTime = lastEditedTime;
