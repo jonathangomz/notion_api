5c0298ca94e73abc1a0ec1fbd81ace65d5cc4708
jonathangomz <jonicgp97@gmail.com>
2022-02-23 22:13:34 -0600

    Renames BaseFields to Object

--- test/general_test.dart
+++ test/general_test.dart
@@ -1,11 +1,11 @@
 import 'package:notion_api/notion_api.dart';
-import 'package:notion_api/src/notion/objects/base_fields.dart';
+import 'package:notion_api/src/notion/objects/object.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Base fields tests =>', () {
     test('Create an instance from json', () {
-      BaseFields base = BaseFields.fromJson({
+      Object base = Object.fromJson({
         "object": "database",
         "id": "386da3c6-46bb-4581-8807-1fdb2fbbf447",
         "created_time": "2021-05-19T20:21:11.420Z",
